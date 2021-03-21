e5840a3281e9a1d955de8bfff1374d15c97c6e35
shenhao <disklight@outlook.com>
2021-03-21 23:36:15 +0800

    添加项目文件。

---
+++ fluxx/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace fluxx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.baoShan1880CoilYardDataSet = new fluxx.BaoShan1880CoilYardDataSet();
+            this.baoShan1880CoilYardDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ProductBId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baoShan1880CoilYardDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baoShan1880CoilYardDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductBId,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView.Location = new System.Drawing.Point(27, 63);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 27;
+            this.dataGridView.Size = new System.Drawing.Size(1486, 521);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // baoShan1880CoilYardDataSet
+            // 
+            this.baoShan1880CoilYardDataSet.DataSetName = "BaoShan1880CoilYardDataSet";
+            this.baoShan1880CoilYardDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // baoShan1880CoilYardDataSetBindingSource
+            // 
+            this.baoShan1880CoilYardDataSetBindingSource.DataSource = this.baoShan1880CoilYardDataSet;
+            this.baoShan1880CoilYardDataSetBindingSource.Position = 0;
+            // 
+            // ProductBId
+            // 
+            this.ProductBId.HeaderText = "卷号";
+            this.ProductBId.Name = "ProductBId";
+            this.ProductBId.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "盒式打包";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "切割";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "打包";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "修磨";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "切割";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "称重";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1525, 596);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baoShan1880CoilYardDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baoShan1880CoilYardDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private BaoShan1880CoilYardDataSet baoShan1880CoilYardDataSet;
+        private System.Windows.Forms.BindingSource baoShan1880CoilYardDataSetBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductBId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ fluxx/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace fluxx
+{
+    public partial class Form1 : Form
+    {
+        string strConn = ConfigurationManager.ConnectionStrings["fluxx.Properties.Settings.BaoShan1880CoilYardConnectionString"].ToString();
+        DataTable dt = null;
+        List<List<string>> result = new List<List<string>>();
+        List<string> temp = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //设置数据表格为只读
+            dataGridView.ReadOnly = true;
+            //不允许添加行
+            dataGridView.AllowUserToAddRows = false;
+            using (SqlConnection conn = new SqlConnection(strConn)) //定义连接对象并实例化（实例化时需要提供连接字符串作为参数）
+            {
+                if (conn.State == ConnectionState.Closed) //判断数据库连接是否已经打开，如果没打开则打开数据库连接
+                {
+                    conn.Open(); //打开数据库连接
+                }
+                string sql = "SELECT OPERATION.ProductBId, OPERATIONLIST.BId FROM ProductOperation AS OPERATION INNER JOIN ProductOperationList AS OPERATIONLIST ON OPERATION.ListId = OPERATIONLIST.Id";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dt = new DataTable();
+                        dt.Load(dr);
+                    }
+                }
+            }
+            if (dt != null)
+            {
+                temp.Add(dt.Rows[0][0].ToString());
+                for (int i = 0; i < dt.Rows.Count; i++) //行
+                {
+
+                    if (i > 0 && dt.Rows[i][0].ToString() != dt.Rows[i - 1][0].ToString())
+                    {
+
+                        result.Add(temp);
+                        temp = new List<string>
+                        {
+                            dt.Rows[i][0].ToString()
+                        };
+                    }
+                    temp.Add(dt.Rows[i][1].ToString());
+                }
+                result.Add(temp);
+                //dataGridView.Rows.Add()
+            }
+            if (result.Any())
+            {
+                var type = "";
+                for(int i = 0;i<result.Count;i++)
+                {
+                    int index = dataGridView.Rows.Add();
+                    for (int j = 0; j < result[i].Count; j++)
+                    {
+                        
+                        dataGridView.Rows[index].Cells[0].Value = result[i][0];
+                        type = result[i][j];
+                        switch(type)
+                        {
+                            case "Boxing":
+                                dataGridView.Rows[index].Cells[1].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            case "Cutting":
+                                dataGridView.Rows[index].Cells[2].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            case "Packaging":
+                                dataGridView.Rows[index].Cells[3].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            case "Repair":
+                                dataGridView.Rows[index].Cells[4].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            case "Sampling":
+                            case "SamplingOnPackaging":
+                                dataGridView.Rows[index].Cells[5].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            case "Weighing":
+                                dataGridView.Rows[index].Cells[6].Style.BackColor = Color.FromArgb(86, 156, 214);
+                                break;
+                            
+                        }
+                        //dataGridView.Rows[index].Cells[1].Value = true;
+                    }
+                }
+                
+            }
+        }
+    }
+}
